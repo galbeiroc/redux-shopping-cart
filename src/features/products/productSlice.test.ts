23156c2a714bed12a1c326afa8d317044a57fc4b
galbeiroc <alcresgu_1989@hotmail.com>
2022-04-17 15:53:23 -0500

    test not allow same product and multiple products

--- src/features/products/productSlice.test.ts
+++ src/features/products/productSlice.test.ts
@@ -17,4 +17,24 @@ describe('products reducer', () => {
       expect(result.products[product.id]).toEqual(product)
     });
   });
+  it('should not allow the same product to be added more than once', () => {
+    const initialState = undefined;
+    const action = receivedProducts(products);
+    let result = productsReducer(initialState, action);
+    expect(Object.keys(result.products).length).toEqual(products.length);
+    products.forEach((product) => {
+      expect(result.products[product.id]).toEqual(product)
+    });
+    result = productsReducer(result, action);
+    expect(Object.keys(result.products).length).toEqual(products.length);
+  });
+  it('should allow multiple products to be recived at diffent times', () => {
+    const initialState = undefined;
+    const action = receivedProducts(products.slice(0, 2));
+    let result = productsReducer(initialState, action);
+    expect(Object.keys(result.products).length).toEqual(2);
+    const secondAction = receivedProducts(products.slice(2, 4));
+    result = productsReducer(result, secondAction);
+    expect(Object.keys(result.products).length).toEqual(4);
+  });
 });
