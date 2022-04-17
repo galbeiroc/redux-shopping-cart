de4d538e763051d42db2bb503654cf6688ce08b5
galbeiroc <alcresgu_1989@hotmail.com>
2022-04-17 15:06:33 -0500

    test products received

--- src/features/products/productSlice.test.ts
+++ src/features/products/productSlice.test.ts
@@ -1,4 +1,5 @@
 import productsReducer, { receivedProducts } from "./productSlice";
+import products from '../../../public/products.json';
 
 describe('products reducer', () => {
   it('should return the initial state when passed an empty action', () => {
@@ -6,5 +7,14 @@ describe('products reducer', () => {
     const action = { type: "" };
     const result = productsReducer(initialState, action);
     expect(result).toEqual({ products: {} });
+  });
+  it('should convert the products received to an object', () => {
+    const initialState = undefined;
+    const action = receivedProducts(products);
+    const result = productsReducer(initialState, action);
+    expect(Object.keys(result.products).length).toEqual(products.length);
+    products.forEach((product) => {
+      expect(result.products[product.id]).toEqual(product)
+    });
   });
 });
